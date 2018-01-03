2d29663c981e4ec0a0bd67ab5567ff46a5def17f
cho in-tack <34809813+IMCHO@users.noreply.github.com>
2018-01-03 23:39:02 +0900

    CH02_Day1

---
+++ 2.py
@@ -0,0 +1,8 @@
+a=[1,3,5,4,2]
+a.sort()
+a.reverse()
+print(a)
+
+b=['Life','is','too','short']
+result=" ".join(b)
+print(result)
---
+++ 3.py
@@ -0,0 +1,8 @@
+a=(1,2,3)
+a=a+(4,)
+print(a)
+
+b={'A':90,'B':80,'C':70}
+result=b.pop('B')
+print(b)
+print(result)
